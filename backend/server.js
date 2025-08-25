c99d599210e83cb89a83a61a31a619d96b4a4e70
Rohit Tak <rohitmtak@gmail.com>
2025-08-25 13:16:12 +0530

    chore: add Netlify config and fix fiber peer deps

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,9 @@ const initializeApp = async () => {
                     'http://localhost:3000', 
                     'http://localhost:5174',
                     'http://127.0.0.1:5173',
-                    'http://127.0.0.1:5174'
+                    'http://127.0.0.1:5174',
+                    // Netlify domain
+                    'https://highstreetpix.netlify.app'
                 ];
                 
                 if (allowedOrigins.indexOf(origin) !== -1) {
